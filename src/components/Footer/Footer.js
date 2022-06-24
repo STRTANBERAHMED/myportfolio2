3d2e77d86e5f02ef48201066ff1294101d4b4e77
strtanberahmed <tanber1111@gmail.com>
2022-06-24 20:08:04 +0300

    design updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -57,8 +57,8 @@ const Footer = () => {
                             <ul>
                                 <h5 className='socials'>Socials</h5>
                                 <a href="https://medium.com/@md.tanber4433"><i className="lg bx bxl-medium-square"></i></a>
-                                <a href="https://www.instagram.com/strtanber/"><i className="lg bx bxl-twitter"></i></a>
-                                <a href="https://twitter.com/StrTanber"><i className="lg bx bxl-instagram"></i></a>
+                                <a href="https://www.instagram.com/strtanber/"><i className="lg bx bxl-instagram"></i></a>
+                                <a href="https://twitter.com/StrTanber"><i className="lg bx bxl-twitter"></i></a>
                             </ul>
                         </div>
                     </div>
